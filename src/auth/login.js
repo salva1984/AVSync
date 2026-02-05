4b203ad82483aea521e403167b315251fab3c990
“sgmunoz1” <sgmunoz@espol.edu.ec>
2026-02-04 23:28:41 -0500

    feat: almacenamiento y creacion de sesiones

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,53 +1,45 @@
-import { chromium } from "playwright";
 import terminalImage from 'terminal-image';
-import readline from 'node:readline';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-export async function login() {
+export async function login(page) {
 
     let numeroAutenthicator;
-
-    const browser = await chromium.launch({ headless: true });
-    const page = await browser.newPage();
 
     await page.goto('https://aulavirtual.espol.edu.ec/login/openid_connect');
     try {
         await page.waitForURL('https://sts.espol.edu.ec/adfs/ls/**');
 
-        await page.getByPlaceholder('usuario@espol.edu.ec').fill(process.env.EMAIL);
-
         //password cambia dependiendo de el idioma
+        await page.getByPlaceholder('usuario@espol.edu.ec').fill(process.env.EMAIL);
         await page.locator('#passwordInput').fill(process.env.PASSWORD);
         await page.locator('#submitButton').click();
 
         await page.waitForURL('https://login.microsoftonline.com/login.srf');
-        
+
         // confiar en espol.edu.ec
         await page.locator('#idSIButton9').click();
 
         await page.waitForURL('https://login.microsoftonline.com/appverify');
         // esperamos esa animacion
         await page.waitForTimeout(2000);
-        const buffer = await page.locator('.display-sign-container').screenshot();
+
         // muestra el numero de auth en consola
+        const buffer = await page.locator('.display-sign-container').screenshot();
         console.log(await terminalImage.buffer(buffer));
 
-        await page.getByRole('input', { value: 'Yes' }).click();
+        // guardar sesion -> si
+        await page.locator('#idSIButton9').click();
         console.log(page.url());
+
+       
+        await page.context().storageState({ path:'cookies.json' })
+        console.log('Cookies saved!');
+
+        return page;
 
     } catch (error) {
         const buffer = await page.screenshot({ path: 'error.png', fullPage: true });
         console.log(await terminalImage.buffer(buffer));
         throw error;
     }
-
-
-
-
-
 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,60 @@
-import { login } from "./auth/login.js";
 import dotenv from 'dotenv';
+import { access } from "fs/promises";
+import * as fs from 'node:fs/promises';
+import path from "path";
+import { chromium } from "playwright";
+import terminalImage from 'terminal-image';
+import { fileURLToPath } from "url";
+import { login } from "./auth/login.js";
+import { errors } from "playwright";
+
+
+export const PROJECT_ROOT = path.resolve(
+    path.dirname(fileURLToPath(import.meta.url)),
+    ".."
+)
+const cookiesPath = path.join(PROJECT_ROOT, "cookies.json");
+const hasCookies = await fileExists(cookiesPath);
 
 dotenv.config();
 
-await login();
+const browser = await chromium.launch({ headless: true });
+
+const context = await createContext();
+
+const page = await context.newPage();
+
+await page.goto('https://aulavirtual.espol.edu.ec/');
+
+try {
+    await page.waitForSelector(".bienvenidos", { timeout: 5000 })
+    hasCookies ? console.log("► Sesion expirada, iniciando sesion") : console.log("► Iniciando sesion");
+    await login(page);
+} catch (error) {
+    if (error instanceof errors.TimeoutError) {
+        console.log("► Cookies validas");
+    } else {
+        console.log(error);
+        throw error;
+    }
+}
+
+
+async function createContext() {
+    if (hasCookies) {
+        console.log("► Cookies encontradas");
+        return browser.newContext({ storageState: cookiesPath });
+    }
+    console.log("► No hay cookies");
+    return browser.newContext();
+}
+
+async function fileExists(path) {
+    try {
+        await access(path, fs.F_OK);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
